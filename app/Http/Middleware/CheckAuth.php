27440048eb9e0ec4810891a72979f3b21c03b5e2
Ruben <rvazquezcaceres@gmail.com>
2019-12-10 14:37:31 +0100

    Model user created, controller created, checkAuth

---
+++ app/Http/Middleware/CheckAuth.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use App\User;
+
+class CheckAuth
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $user = new User();
+
+        if($user->is_authorized($request))
+        {
+            $token_header = $request->header('Authorization');
+            $token = new Token();
+            $data = $token->decode($token_header);
+            $request->request->add(['data_token' => $data]);
+
+            return $next($request);
+        }
+        
+        var_dump('no tienes permisos'); exit;
+    }
+}
