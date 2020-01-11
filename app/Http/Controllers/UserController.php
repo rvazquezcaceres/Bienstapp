f7a8c884b33b9cc11d128a5f1cd8e5547a30f580
Ruben <rvazquezcaceres@gmail.com>
2020-01-11 20:40:33 +0100

    User controller update

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 use App\User;
 use App\Helpers\Token;
 
-class UserController extends Controller
+class userController extends Controller
 {
     /**
      * Display a listing of the resource.
@@ -17,7 +17,6 @@ class UserController extends Controller
     {
         //
     }
-
     /**
      * Show the form for creating a new resource.
      *
@@ -27,7 +26,6 @@ class UserController extends Controller
     {
         //
     }
-
     /**
      * Store a newly created resource in storage.
      *
@@ -36,33 +34,41 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
+        
         $user = new User();
-        $user->register($request);
-        $data_token = [
-            "email" => $user->email,
-        ];
-        $token = new Token($data_token);
-        $token_encode = $token->encode();
-        return response()->json([
-            "token" => $token_encode
-        ], 201);
+        if (!$user->userExists($request->email)){
+            $user->register($request);
+            $data_token = [
+                "email" => $user->email,
+            ];
+            $token = new Token($data_token);
+            
+            $tokenEncoded = $token->encode();
+            return response()->json([
+                "token" => $tokenEncoded
+            ], 201);
+        }else{
+            return response()->json(["Error" => "No se pueden crear usuarios con el mismo Email o con el Email vacío"]);
+        }
     }
-
     /**
      * Display the specified resource.
      *
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
-    {
+    public function show(Request $request)
+    {        
         $email = $request->data_token->email;
-        $user = User::where('email', $email)->first(); 
-        return response()->json([
-            "User" => $user
-        ], 200);
+        $user = User::where('email',$email)->first();
+        if(isset($user)){
+            $user->password = decrypt($user->password);
+            return response()->json(["Success" => $user]);
+        }else{
+            return response()->json(["Error" => "El ususario no existe"]);
+        }
+            
     }
-
     /**
      * Show the form for editing the specified resource.
      *
@@ -73,7 +79,6 @@ class UserController extends Controller
     {
         //
     }
-
     /**
      * Update the specified resource in storage.
      *
@@ -83,82 +88,83 @@ class UserController extends Controller
      */
     public function update(Request $request, $id)
     {
-        $email = $request->data_token->email;
-        $user = User::where('email', $email)->first();
-        $user->name = $request->name;
-        $user->email = $request->email;
-        $user->password = $request->password;
-        $user->update();
+        $user = user::where('email',$request->data_token->email)->first();
+        if (isset($user)) {
+            
+            $user->name = $request->name;
+            $user->password = $request->password;
+            $user->update();
+        
+            return response()->json(["Success" => "Se ha modificado el usuario."]);
+        }else{
+            return response()->json(["Error" => "El usuario no existe"]);
+        }
     }
-
     /**
      * Remove the specified resource from storage.
      *
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Request $request)
     {
         $email = $request->data_token->email;
-        $user = User::where('email', $email)->first();
-        $user->delete();
-            return response()->json([
-                "message" => 'el usuario ha sido eliminado'
-            ], 200);
-    }
-
-    public function login(Request $request)
-    {
-        $data_token = [
-            'email' => $request->email,
-        ];
-        $user = User::where($data_token)->first();
-
-        if($user->password == $request->password)
-        {
-            $token = new Token($data_token);
-            $token_encode = $token->encode();
-            return response()->json([
-                "token" => $token_encode
-            ], 200);
+        $user = User::where('email',$email)->first();
+        if (isset($user)) {            
+            if ($request->password == decrypt($user->password)) {
+               $user->delete();            
+                return response()->json(["Success" => "Se ha borrado el usuario."]);
+            }else{
+                return response()->json(["Error" => "la contraseña no coincide con la del usuario"]);
+            }
+        }else{
+            return response()->json(["Error" => "El ususario no existe"]);
         }
-        return response()->json([
-            "message" => "Unauthorized"
-        ], 401);
     }
-
-    public function password_recover(Request $request)
-    {
-        $data_token = [
-            "email" => $request->email
-        ];
-        $user = User::where('email', $request->email)->first();
-        //var_dump($user->email);exit;
-        if(isset($user))
-        {
-            $number_rand = rand(10000, 99999);
-            $array = ['#','@','!','$','%','&','?','¿','¡','!'];
-            // mirar aleatorio de 0 a $array.count, insertar en vez de #
-            $newPassword = "ChangePassword".$number_rand."#";
-            //var_dump($newPassword);exit();
-            $message = ("La nueva contraseña es: " . $number_rand);
-            $to = $user->email;
-            $titulo = 'recuperar contraseña';
-            $cabeceras = 'From: recuperar_contraseñas@cev.com'. "\r\n" . 'Replay-To: $user->email' . "\r\n" . 'X-Mailer: PHP/' . phpversion();
-            mail($to, $titulo, $message, $cabeceras);
-
-            $user->password = $number_rand;
-            $user->update();
-
-            return response()->json([
-                "message" => 'Email enviado'
-            ], 200);
-        } 
-        else 
-        {
-            return response()->json([
-                "message" => 'Codigo erroneo'
-            ], 200);
+    public function login(Request $request){
+        $data_token = ['email'=>$request->email];
+        
+        $user = User::where($data_token)->first();  
+       
+        if ($user!=null) {       
+            if($request->password == decrypt($user->password))
+            {       
+                $token = new Token($data_token);
+                $tokenEncoded = $token->encode();
+                return response()->json(["token" => $tokenEncoded], 201);
+            }   
+        }     
+        return response()->json(["Error" => "No se ha encontrado"], 401);
+    }
+    public function recoverPassword (Request $request){
+        $user = User::where('email',$request->email)->first();  
+        if (isset($user)) {   
+            $newPassword = self::randomPassword();
+            self::sendEmail($user->email,$newPassword);
+            
+                $user->password = $newPassword;
+                $user->update();
+            
+            return response()->json(["Success" => "Se ha restablecido su contraseña, revise su correo electronico."]);
+        }else{
+            return response()->json(["Error" => "El Email no existe"]);
         }
+    }
+    public function sendEmail($email,$newPassword){
+        $para      = $email;
+        $titulo    = 'Recuperar contraseña de Bienestapp';
+        $mensaje   = 'Se ha establecido "'.$newPassword.'" como su nueva contraseña.';
+        mail($para, $titulo, $mensaje);
+    }
+    
+    public function randomPassword() {
+        $alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
+        $pass = array(); //remember to declare $pass as an array
+        $alphaLength = strlen($alphabet) - 1; //put the length -1 in cache
+        for ($i = 0; $i < 10; $i++) {
+            $n = rand(0, $alphaLength);
+            $pass[] = $alphabet[$n];
+        }
+        return implode($pass); //turn the array into a string
     }
 }
